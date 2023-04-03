4551a2d1853dd1781aed53fdb6be16e2d3d8e96f
damiansobecki <damian.sobec@gmail.com>
2023-04-03 14:54:55 +0200

    testing

--- src/main/java/com/raimhied/cbcinterpreter/question/PageController.java
+++ src/main/java/com/raimhied/cbcinterpreter/question/PageController.java
@@ -55,9 +55,10 @@ public class PageController {
 
         System.out.println(this.user.toString());
 
-        // userService.saveUser(user);
+        userService.saveUser(user); // test
 
         return "page";
     }
 
 }
+
